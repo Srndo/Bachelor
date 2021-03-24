61d7731ccbce801e6dc3ee9b393fcb0079011fbc
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-03-24 11:35:25 +0100

    DA: added fillWithData()

--- DatabaseArchive+CoreDataProperties.swift
+++ DatabaseArchive+CoreDataProperties.swift
@@ -23,6 +23,32 @@ extension DatabaseArchive {
     @NSManaged public var protoID: Int16
     @NSManaged public var recordID: String?
     @NSManaged public var construction: String
+    
+    func fillWithData(encodedProto: String, local: Bool, recordID: String? = nil) -> Proto? {
+        guard let data = Data(base64Encoded: encodedProto) else { printError(from: "fillWithData", message: "Cannot convert encodedProto to data"); return nil }
+        printError(from: "fillWithData", message: "Cannot decode encodedProto to Proto")
+        guard let proto = try? JSONDecoder().decode(Proto.self, from: data) else { printError(from: "fillWithData", message: "Cannot decode encodedProto to Proto"); return nil }
+        
+        fill(proto: proto, local: local, recordID: recordID)
+        return proto
+    }
+    
+    func fillWithData(proto: Proto, local: Bool, recordID: String? = nil) -> String?{
+        guard let encoded = try? JSONEncoder().encode(proto).base64EncodedString() else { printError(from: "fillWithData", message: "Cannot encode protocol[\(proto.id)]"); return nil}
+        self.encodedProto = encoded
+        fill(proto: proto, local: local, recordID: recordID)
+        
+        return encoded
+    }
+    
+    private func fill(proto: Proto, local: Bool, recordID: String?) {
+        self.client = proto.client.name
+        self.date = proto.creationDate
+        self.local = local
+        self.protoID = Int16(proto.id)
+        self.construction = proto.construction.name
+        self.recordID = recordID
+    }
 
 }
 
