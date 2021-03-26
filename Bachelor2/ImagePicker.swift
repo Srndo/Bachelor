62bddd0f1265687de72220abedd2855e1a859fc3
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-03-26 12:49:40 +0100

    imagePicker

---
+++ Bachelor2/ImagePicker.swift
@@ -0,0 +1,65 @@
+//
+//  ImagePicker.swift
+//  Bachelor2
+//
+//  Created by Simon Sestak on 30/10/2020.
+//  Copyright © 2020 Simon Sestak. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+class ImagePickerCoordinator: NSObject, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    @Environment(\.managedObjectContext) var moc
+    @Binding var isShow: Bool
+    @Binding var photos: [MyPhoto]
+    var index: Int
+    var protoID: Int
+    
+    init(isShow: Binding<Bool>, photos: Binding<[MyPhoto]>, protoID: Int, lastPhotoIndex: Binding<Int>) {
+        _isShow = isShow
+        _photos = photos
+        index = lastPhotoIndex.wrappedValue + 1
+        self.protoID = protoID
+        lastPhotoIndex.wrappedValue = lastPhotoIndex.wrappedValue + 1
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        DispatchQueue.global().async {
+            let uiimage = info[.originalImage] as? UIImage
+            let photo = MyPhoto(context: self.moc)
+            photo.savePhotoToDisk(photo: uiimage, protoID: self.protoID, number: self.index, value: -1.0)
+            DispatchQueue.main.async {
+                self.photos.append(photo)
+            }
+        }
+        isShow = false
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        isShow = false
+    }
+}
+
+struct ImagePicker: UIViewControllerRepresentable{
+    @Binding var isShow: Bool
+    @Binding var photos: [MyPhoto]
+    @Binding var lastPhotoIndex: Int
+    var protoID: Int
+    
+    var source: UIImagePickerController.SourceType
+    
+    func updateUIViewController(_ uiViewController: UIImagePickerController, context: UIViewControllerRepresentableContext<ImagePicker>) {
+    }
+    
+    func makeCoordinator() -> ImagePickerCoordinator {
+        return ImagePickerCoordinator(isShow: $isShow, photos: $photos, protoID: protoID, lastPhotoIndex: $lastPhotoIndex)
+    }
+    
+    func makeUIViewController(context: UIViewControllerRepresentableContext<ImagePicker>) -> UIImagePickerController {
+        let picker = UIImagePickerController()
+        picker.sourceType = source
+        picker.delegate = context.coordinator
+        return picker
+    }
+}
