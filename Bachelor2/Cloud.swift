de80eec758ab03d2bfeb678beb3baaa54669631c
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-03-26 12:45:05 +0100

    fetchPhoto, savePhoto

--- Bachelor2/Cloud.swift
+++ Bachelor2/Cloud.swift
@@ -13,6 +13,13 @@ struct CloudResultEncodedProto {
     var encodedProto: String?
 }
 
+struct CloudResultPhoto {
+    var recordID: CKRecord.ID
+    var protoID: Int
+    var value: Double
+    var name: Int
+}
+
 struct CloudResultZip {
     var recordID: CKRecord.ID
     var zip: String
@@ -27,6 +34,7 @@ struct Cloud {
     
     struct RecordType {
         static let protocols = "Protocols"
+        static let zip = "Outputs"
         static let photos = "Photos"
     }
     
@@ -38,10 +46,135 @@ struct Cloud {
         case assetFailure
     }
     
+    static func savePhoto(protoID: Int, value: Double, name: Int, path: URL, completition: @escaping (Result<CloudResultPhoto, Error>) -> ()) {
+        let recordID = CKRecord.ID( zoneID: CloudElements.zone.zoneID)
+        let itemRecord = CKRecord(recordType: RecordType.photos, recordID: recordID)
+        itemRecord["protoID"] = protoID as CKRecordValue
+        itemRecord["value"] = value as CKRecordValue
+        itemRecord["name"] = name as CKRecordValue
+        
+        let asset = CKAsset(fileURL: path)
+        itemRecord["photo"] = asset
+        
+        CloudElements.container.privateCloudDatabase.save(itemRecord) { record , err in
+            DispatchQueue.main.async {
+                if let err = err {
+                    completition(.failure(err))
+                    return
+                }
+                
+                guard let record = record else {
+                    completition(.failure(CloudKitHelperErrors.recordFailure))
+                    return
+                }
+                
+                let recordID = record.recordID
+                
+                guard let protoID = record["protoID"] as? Int else {
+                    completition(.failure(CloudKitHelperErrors.castFailure))
+                    return
+                }
+                
+                guard let value = record["value"] as? Double else {
+                    completition(.failure(CloudKitHelperErrors.castFailure))
+                    return
+                }
+                
+                guard let _ = record["photo"] as? CKAsset else {
+                    completition(.failure(CloudKitHelperErrors.assetFailure))
+                    return
+                }
+                
+                guard let name = record["name"] as? Int else {
+                    completition(.failure(CloudKitHelperErrors.castFailure))
+                    return
+                }
+                
+                let element = CloudResultPhoto(recordID: recordID, protoID: protoID, value: value, name: name)
+                completition(.success(element))
+            }
+        }
+        
+    }
+    
+    static func fetchPhoto(completition: @escaping (Result<CloudResultPhoto, Error>) -> ()) {
+        let predicate = NSPredicate(value: true)
+        let sort = NSSortDescriptor(key: "creationDate", ascending: true)
+        
+        let querry = CKQuery(recordType: RecordType.photos, predicate: predicate)
+        querry.sortDescriptors = [sort]
+        
+        let operation = CKQueryOperation(query: querry)
+        operation.desiredKeys = ["protoID", "value", "photo", "name"]
+        operation.resultsLimit = 50
+        
+        operation.recordFetchedBlock = { record in
+            DispatchQueue.main.async {
+                
+                let recordID = record.recordID
+                
+                guard let protoID = record["protoID"] as? Int else {
+                    completition(.failure(CloudKitHelperErrors.castFailure))
+                    return
+                }
+                
+                guard let value = record["value"] as? Double else {
+                    completition(.failure(CloudKitHelperErrors.castFailure))
+                    return
+                }
+                
+                guard let asset = record["photo"] as? CKAsset else {
+                    completition(.failure(CloudKitHelperErrors.assetFailure))
+                    return
+                }
+                
+                guard let name = record["name"] as? Int else {
+                    completition(.failure(CloudKitHelperErrors.castFailure))
+                    return
+                }
+                
+                guard let photoURL = asset.fileURL else {
+                    completition(.failure(CloudKitHelperErrors.assetFailure))
+                    return
+                }
+                
+                guard let data = try? Data(contentsOf: photoURL) else {
+                    completition(.failure(CloudKitHelperErrors.assetFailure))
+                    return
+                }
+                
+                DispatchQueue.global().async {
+                    do {
+                        guard let imagePath = Dirs.shared.getSpecificPhotoDir(protoID: protoID) else { return }
+                        try data.write(to: imagePath.appendingPathComponent(String(name)))
+                    } catch {
+                        printError(from: "fetch photo", message: error.localizedDescription)
+                        return
+                    }
+                }
+                
+                let element = CloudResultPhoto(recordID: recordID, protoID: protoID, value: value, name: name)
+                
+                print("Photo fetched")
+                completition(.success(element))
+            }
+        }
+        
+        operation.queryCompletionBlock = { (_, err) in
+            DispatchQueue.main.async {
+                if let err = err {
+                    completition(.failure(err))
+                    return
+                }
+            }
+        }
+        
+    }
+    
     // MARK: TODO
     static func saveZIP(protoID: Int, zip: String, completition: @escaping (Result<CloudResultZip, Error>) -> ()) {
         let recordID = CKRecord.ID( zoneID: CloudElements.zone.zoneID)
-        let itemRecord = CKRecord(recordType: RecordType.protocols, recordID: recordID)
+        let itemRecord = CKRecord(recordType: RecordType.zip, recordID: recordID)
         itemRecord["protoID"] = protoID as CKRecordValue
         itemRecord["zip"] = zip as CKRecordValue // check if as CKRecordValue
         
@@ -81,7 +214,7 @@ struct Cloud {
         let predicate = NSPredicate(value: true)
         let sort = NSSortDescriptor(key: "creationDate", ascending: true)
         
-        let querry = CKQuery(recordType: RecordType.photos, predicate: predicate)
+        let querry = CKQuery(recordType: RecordType.zip, predicate: predicate)
         querry.sortDescriptors = [sort]
         
         let operation = CKQueryOperation(query: querry)
