53279406e8ba0b3b112f4af4d95054d1c8b6de20
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-04-01 20:44:36 +0200

    Dirs: Remove dir and comments

--- Bachelor2/DirsHelper.swift
+++ Bachelor2/DirsHelper.swift
@@ -11,30 +11,45 @@ class Dirs {
     static let shared = Dirs()
     private let fileManager = FileManager.default
     
+    /**
+        Return urls like .../Documents
+     */
     private func getDocumentsDir() -> URL? {
         let documentsDirectory = fileManager.urls(for: .documentDirectory, in: .userDomainMask).first
         guard let docURL = documentsDirectory else { printError(from: "getDocumentsDir", message: "Documents directory is nil"); return nil}
         return docURL
     }
     
+    /**
+        Return urls like .../Documents/Images
+     */
     func getImagesDir() -> URL? {
         guard let docURL = getDocumentsDir() else { return nil }
         let imagesURL = docURL.appendingPathComponent("Images")
         return imagesURL
     }
     
+    /**
+        Return urls like .../Documents/Documents
+     */
     func getProtosDir() -> URL? {
         guard let docURL = getDocumentsDir() else { return nil }
         let protosURL = docURL.appendingPathComponent("Documents")
         return protosURL
     }
 
+    /**
+        Return urls like .../Output
+     */
     func getOutputDir() -> URL? {
         guard let docURL = getDocumentsDir() else { return nil }
         let outputURL = docURL.appendingPathComponent("Output")
         return outputURL
     }
     
+    /**
+        Return urls like .../Documents/Output/{protoID}
+     */
     func getProtocolOutputDir(protoID: Int) -> URL? {
         guard let docURL = getOutputDir() else { return nil }
         let outputURL = docURL.appendingPathComponent(String(protoID))
@@ -48,6 +63,9 @@ class Dirs {
         return outputURL
     }
     
+    /**
+        Return urls like .../Documents/Images/{protoID}
+     */
     func getSpecificPhotoDir(protoID: Int) -> URL? {
         guard let docURL = getImagesDir() else { return nil }
         let imagesURL = docURL.appendingPathComponent(String(protoID))
@@ -82,6 +100,9 @@ class Dirs {
         }
     }
     
+    /**
+        Return urls like .../Documents/Output/{protoID}/{internalID}
+     */
     func getSpecificOutputDir(protoID: Int, internalID: Int) -> URL? {
         guard let outputURL = getProtocolOutputDir(protoID: protoID) else { return nil }
         let specific = outputURL.appendingPathComponent(String(internalID))
@@ -89,8 +110,23 @@ class Dirs {
         return specific
     }
     
+    /**
+        Return urls like .../Documents/Output/{protoID}/{internalID}/photos.zip
+     */
     func getZipURL(protoID: Int, internalID: Int) -> URL? {
         guard let specific = getSpecificOutputDir(protoID: protoID, internalID: internalID) else { return nil }
         return specific.appendingPathComponent("photos.zip")
+    }
+    
+    //MARK: TODO not deleting
+    func removeDir(at: URL?) -> Bool {
+        guard let path = at else { return true }
+        do {
+            try fileManager.removeItem(at: path)
+        } catch {
+            printError(from: "remove Dir", message: error.localizedDescription)
+            return false
+        }
+        return true
     }
 }
