5a3d4ba063d98c0a285d0a3341196fea4d900ec2
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-04-01 21:01:49 +0200

    Extensions:  - ListProtoView func into extensions  - delete local photos on creating ZIP

--- Bachelor2/Extensions.swift
+++ Bachelor2/Extensions.swift
@@ -80,6 +80,7 @@ extension ProtocolView {
             self.ico = ""
             self.dic = ""
             self.reqVal = ""
+            self.photos = []
             self.message = "Protokol uložený"
         }
     }
@@ -111,6 +112,7 @@ extension ProtocolView {
                     self.ico = String(proto.client.ico)
                     self.dic = String(proto.client.dic)
                     self.reqVal = String(proto.method.requestedValue)
+                    self.lastPhotoNumber = proto.lastPhotoIndex
                     self.internalID = proto.internalID
                 }
             } else {
@@ -136,9 +138,10 @@ extension ProtocolView {
         self.proto.internalID = proto.internalID + 1
         self.internalID = proto.internalID
         
+        // MARK: TODO: wait until document saved
+        // add closure and into this closure createProtoPDF, and Cloud.shared.saveToCloud(pdf) <--- [cloud pretaz fce, same params except last]
         modify() // save incremented proto.id
         
-        // MARK: TODO: wait until document saved
         guard let zipURL = createPhotosZIP(protoID: protoID) else { return }
         Cloud.shared.saveToCloud(recordType: Cloud.RecordType.outputs, protoID: proto.id, internalID: proto.internalID, pathTo: zipURL){ _ in}
         createProtoPDF(protoID: protoID)
@@ -168,11 +171,14 @@ extension ProtocolView {
             return  nil
         }
 
-        // MARK: TODO: Remove local copy of those photos
-//        for photo in self.photos {
-//            photo.deleteFromDisk()
-//        }
-        print("ZIP with photos of protocol \(self.proto.id) was created")
+        // MARK: Remove local photo
+        for photo in self.photos {
+            photo.deleteFromDisk()
+            photo.local = false
+            Cloud.shared.modifyOnCloud(photo: photo)
+        }
+        moc.trySave(errorFrom: "create ZIP from photos", error: "Cannot saved remove changes [photo.local -> false]")
+        print("ZIP with photos of protocol \(self.proto.id) was created at path\n\(zipURL)")
         return zipURL
     }
     
@@ -210,6 +216,8 @@ extension ProtocolListView {
                 removeDocument(protoID: Int(remove.protoID))
                 moc.delete(remove)
                 moc.trySave(errorFrom: "remove cloud", error: "Cannot saved managed object context")
+                _ = Dirs.shared.removeDir(at: Dirs.shared.getSpecificPhotoDir(protoID: Int(remove.protoID)))
+                _ = Dirs.shared.removeDir(at: Dirs.shared.getProtocolOutputDir(protoID: Int(remove.protoID)))
             }
         }
     }
@@ -228,11 +236,13 @@ extension ProtocolListView {
         }
     }
     
-    func removePhotos(protoID: Int){
-        // MARK: TODO test
+    private func removePhotos(protoID: Int){
         let removes = self.photos.filter{ $0.protoID == protoID }
         for remove in removes {
             remove.deleteFromDisk()
+            if let recordID = remove.recordID {
+                Cloud.shared.deleteFromCloud(recordID: recordID){ _ in}
+            }
             moc.delete(remove)
         }
     }
@@ -267,6 +277,95 @@ extension NSManagedObjectContext {
             printError(from: errorFrom, message: message)
             print(error)
             return
+        }
+    }
+}
+
+
+extension PhotosView {
+    func actionSheet() -> ActionSheet {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            return ActionSheet(title: Text("Urob fotku alebo vyber z kniznice"), message: Text(""), buttons:
+                [.default(Text("Kniznica"), action: {
+                    self.source = .photoLibrary
+                    self.showPicker.toggle()
+                }),
+                 .default(Text("Kamera"), action: {
+                    self.source = .camera
+                    self.showPicker.toggle()
+                 }),
+                .cancel(Text("Zavri"))
+                ]
+            )
+        } else {
+            return ActionSheet(title: Text("Vyber fotku z kniznice"), message: Text(""), buttons:
+                [.default(Text("Kniznica"), action: {
+                    self.source = .photoLibrary
+                    self.showPicker.toggle()
+                }),
+                .cancel(Text("Zavri"))
+                ]
+            )
+        }
+    }
+    
+    func getZipPhotos(zipURL: URL) {
+        guard let imagePath = Dirs.shared.getSpecificPhotoDir(protoID: protoID) else { return }
+        do {
+            try Zip.unzipFile(zipURL, destination: imagePath, overwrite: true, password: nil)
+            insertExtractedPhotosToCoreData(from: imagePath)
+        } catch {
+            printError(from: "getZipPhotos", message: error.localizedDescription)
+            print(error)
+            return
+        }
+    }
+    
+    private func insertExtractedPhotosToCoreData(from: URL){
+        guard let paths = Dirs.shared.getConentsOfDir(at: from) else {
+            printError(from: "extracted photos", message: "Directory is empty\nURL: \(from)")
+            return
+        }
+        var names: [String] = []
+        paths.forEach{ path in
+            let name = path.deletingPathExtension().lastPathComponent
+            names.append(name)
+        }
+        print(names)
+        photos.forEach{ photo in
+            if names.contains(String(photo.name)) {
+                photo.local = true
+            }
+        }
+        moc.trySave(errorFrom: "create photos from ZIP", error: "Cannot saved creating changes [photo.local -> true]")
+    }
+    
+    func deletePhoto(at offsets: IndexSet) {
+        for index in offsets {
+            let remove = photos[index]
+            guard let recordID = remove.recordID else {
+                print("Warning [remove photo]: RecordID of photo \(remove.protoID) is nil")
+                remove.deleteFromDisk()
+                photos.remove(at: index)
+                moc.delete(remove)
+                moc.trySave(errorFrom: "remove cloud", error: "Cannot saved managed object context")
+                return
+            }
+            Cloud.shared.deleteFromCloud(recordID: recordID) { recordID in
+                guard let recordID = recordID else { return }
+                guard let removeCloud = photos.first(where: { $0.recordID == recordID }) else {
+                    printError(from: "remove photo cloud", message: "RecordID returned from cloud not exist in photos contained by proto")
+                    return
+                }
+                guard removeCloud == remove else {
+                    printError(from: "remove cloud", message: "Marked protocol to remove and returned from cloud is not same")
+                    return
+                }
+                remove.deleteFromDisk()
+                photos.remove(at: index)
+                moc.delete(remove)
+                moc.trySave(errorFrom: "remove cloud", error: "Cannot saved managed object context")
+            }
         }
     }
 }
