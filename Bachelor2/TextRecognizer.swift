64289f98dfadfd9d3c3be3b0518f5abc2434855b
Srndo <xsesta06@stud.fit.vutbr.cz>
2021-04-01 20:45:20 +0200

    TextRecognition: working but need flip image <--

--- Bachelor2/TextRecognizer.swift
+++ Bachelor2/TextRecognizer.swift
@@ -8,11 +8,13 @@
 import Vision
 import SwiftUI
 
+// MARK: TODO: Rotated foto to left recognize value
 struct TextRecognizer {
     func regognize(from images: [Int:CGImage], completititon: @escaping ([Int:String]) -> ()) {
         DispatchQueue.global().async {
             var recognizedValues: [Int:String] = [:]
-            var tmp = ""
+            var returnConfidence: Float = 0.5
+            var returnNumber = "-1.0"
             let request = VNRecognizeTextRequest { (request, error) in
                 guard let observations = request.results as? [VNRecognizedTextObservation] else {
                     print("The observations are of an unexpected type.")
@@ -25,24 +27,24 @@ struct TextRecognizer {
                     guard let candidate = observation.topCandidates(maximumCandidates).first else { continue }
                     // check if the first candidate contains numbers
                     print("Confidence \(candidate.confidence)", candidate.string)
-                    guard candidate.confidence >= 0.65 else { continue }
+                    guard candidate.confidence >= returnConfidence else { continue }
                     guard candidate.string.rangeOfCharacter(from: CharacterSet.decimalDigits) != nil else { continue }
+                    returnConfidence = candidate.confidence
+                    returnNumber = candidate.string
                     print("Numbers found: \(candidate.string)")
-                    // add number candidate
-                    tmp += candidate.string
                 }
             }
             
             request.recognitionLevel = .accurate
-            request.usesLanguageCorrection = false
-
+//            request.usesLanguageCorrection = false
+            
             for (name,image) in images {
                 let requestHandler = VNImageRequestHandler(cgImage: image, options: [:])
 
                 do {
                     try requestHandler.perform([request])
-                    recognizedValues[name] = tmp
-                    tmp = ""
+                    recognizedValues[name] = returnNumber
+                    returnNumber = ""
                 } catch {
                     printError(from: "recognize", message: error.localizedDescription)
                 }
